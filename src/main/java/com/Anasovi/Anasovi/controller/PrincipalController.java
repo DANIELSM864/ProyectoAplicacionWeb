02d7ed3ac68e24222be11a2628ceb859351c8a38
DANIELSM864 <danisoleram@gmail.com>
2023-10-31 14:54:44 -0600

    redireccionamiento de index

---
+++ src/main/java/com/Anasovi/Anasovi/controller/PrincipalController.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Anasovi.Anasovi.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/templates")
+public class PrincipalController {
+
+     @GetMapping("/index")
+    public String showIndexPage() {
+        return "index";
+    }
+}
