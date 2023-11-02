4a90fdf3e3479f7073a3d6720ae012743d2e8dfb
DANIELSM864 <danisoleram@gmail.com>
2023-11-02 13:11:02 -0600

    redireccion principal

--- src/main/java/com/Anasovi/Anasovi/controller/PrincipalController.java
+++ src/main/java/com/Anasovi/Anasovi/controller/PrincipalController.java
@@ -14,8 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/templates")
 public class PrincipalController {
 
-     @GetMapping("/index")
+    @GetMapping("/index")
     public String showIndexPage() {
-        return "index";
+        return "index"; 
     }
 }
+
